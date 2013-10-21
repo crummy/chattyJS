fbd5cbd8f04f05c3d1e64cfb11b8b00ebd4d6a5d
Jason Young <r21000@cox.net>
2013-10-21 03:35:39 -0400

    Update 1382340939

--- chatty.js
+++ chatty.js
@@ -1,20 +1,11 @@
 $(function () {
-    'use strict';
-
-    var framerate = 1000 / 30;
+    var FRAMERATE = 1000 / 30;
 
     // Simple little Vector2 class for physics calculations
-    var Vector2 = function _Vector2(x, y) {
+    function Vector2(x, y) {
         this.x = x;
         this.y = y;
-    };
-
-    Vector2.BaseVector =  {
-        get: function () {
-            return new this(0, 0);
-        }
-    };
-
+    }
     // Additional functionality to Array, used if you want to treat it like a stack.
     // Returns the last .push()'d entry... well technically just the last entry which hopefully is the last pushed.
     Array.prototype.front = function () {
@@ -22,34 +13,101 @@ $(function () {
     };
     // Simple assert function to double check my work when parsing replies.
     // Thanks http://stackoverflow.com/questions/15313418/javascript-assert
-    var assert = function _assert(condition, message) {
+    function assert(condition, message) {
         if (!condition) {
             throw message || "Assertion failed";
         }
+    }
+
+    // Applies a force-based calculation to every post.
+    // mostly thanks to http://blog.ivank.net/force-based-graph-drawing-in-as3.html
+    function postCalculationLoop(posts) {
+        posts.forEach(function (postA) {
+            var netVelocity = new Vector2(0, 0);
+            posts.forEach(function(postB) {
+                if (postA === postB) {
+                    return; // no need to compare a node to itself
+                }
+                var repulsion = new Vector2(0, 0),
+                    attraction = new Vector2(0, 0),
+                    gravity = applyGravity.call(postA, postB);
+                if (postA.category === postB.category) {
+                    attraction = attractTo.call(postA, postB);
+                }
+                if (!postA.isSelected()) {
+                    repulsion = repulseFrom.call(postA, postB);
+                }
+                netVelocity.x += repulsion.x + attraction.x + gravity.x;
+                netVelocity.y += repulsion.y + attraction.y + gravity.y;
+            });
+            postA.update(netVelocity);
+        });
     };
 
+    // client for winchatty API. used to get chatty title, post data
+    // browse winchatty here: http://winchatty.com/service/browser/
+    // thanks electroly!
+    function winchatty(args, success, failure) {
+        var postArgs = {},
+            i;
+        for (i = 0; i < args.length; i++) {
+            postArgs['arg' + i] = args[i];
+        }
+        $.ajax({
+            url:      'http://winchatty.com/service/json',
+            type:     'POST',
+            data:     postArgs,
+            dataType: 'json',
+            cache:    false,
+            success:  function (data, textStatus, jqXHR) {
+                if (data.hasOwnProperty('faultString')) {
+                    failure(data.faultString);
+                } else {
+                    success(data);
+                }
+            },
+            error:    function (jqXHR, textStatus, errorThrown) {
+                failure('Failed to call ' + args[0]);
+            }
+        });
+    }
+
+    function Reply() {
+        this.children = [];
+        this.position = new Vector2(Math.random() * Post.prototype.bounds.x, Math.random() * Post.prototype.bounds.y);
+        this.velocity = new Vector2(0, 0);
+        this.div = jQuery('<div/>')
+            .addClass('reply')
+            .appendTo('#chatty');
+    }
+    Reply.prototype.attraction = 0.01;
+    Reply.prototype.repulsion = 600;
+    Reply.prototype.damping = 0.3;
+    Reply.prototype.mass = function () {
+        return 1;
+    }
 
 
-
-
-
-
-    var Post = function _Post() {
+    function Post() {
         var postRef = this;
-
-        postRef.replyList = null;
-        postRef.replyTree = null;
-        postRef.velocity = Vector2.BaseVector;
-        postRef.position = new Vector2(Math.random() * this.bounds.x, Math.random() * this.bounds.y);
-        postRef.isHovered = false;
-        postRef.div = jQuery('<div/>')
+        this.position = new Vector2(Math.random() * this.bounds.x, Math.random() * this.bounds.y);
+        this.replyList = null;
+        this.replyTree = null;
+        this.velocity = new Vector2(0, 0);
+        this.isHovered = false;
+        this.div = jQuery('<div/>')
             .addClass('post')
             .appendTo('#chatty')
             .click(function (e) {
+                postRef.isHovered = false;
                 Post.prototype.selectedPost = (postRef.isSelected() ? null : postRef); // a toggle
                 e.preventDefault();
+            }).mouseenter(function () {
+                if (!postRef.selected) { postRef.isHovered = true; }
+            }).mouseleave(function () {
+                postRef.isHovered = false;
             });
-    };
+    }
     Post.prototype.bounds = new Vector2(800, 600);
     Post.prototype.baseSize = 8;
     Post.prototype.hoveredSizeMultiplier = 2;
@@ -59,19 +117,22 @@ $(function () {
     Post.prototype.repulsion = 600;
     Post.prototype.damping = 0.7;
     Post.prototype.selectedPost = null;
-    Post.prototype.size = function _size() {
+    Post.prototype.size = function () {
         var multiplier = (this.isSelected() ? this.selectedSizeMultiplier : (this.isHovered ? this.hoveredSizeMultiplier : 1));
         return this.baseSize * multiplier + Math.sqrt(this.reply_count);
     };
-    Post.prototype.mass = function _mass() {
+    Post.prototype.mass = function () {
         return this.baseMass + 0.1 * Math.sqrt(this.reply_count);
     };
-    Post.prototype.isSelected = function _isSelected() {
+    Post.prototype.isSelected = function () {
+        assert(this !== null);
         return this === this.selectedPost;
     };
-    Post.prototype.backgroundColour = function _backgroundColour() {
+    Post.prototype.backgroundColour = function () {
         var bgcolour = "black";
-        if (this.category === "ontopic") {
+        if (!this.category) {
+            bgcolour = "black";
+        } else if (this.category === "ontopic") {
             bgcolour = "white";
         } else if (this.category === "offtopic") {
             bgcolour = "lightgrey";
@@ -86,158 +147,137 @@ $(function () {
         }
         return bgcolour;
     };
-    Post.prototype.getOpacity = function _getOpacity() {
+    Post.prototype.getOpacity = function () {
         var isSelected = this.isSelected(),
             noneAreSelected = this.selectedPost === null;
         return ((noneAreSelected || isSelected) ? 1.0 : 0.5);
     };
-    Post.prototype.update = function _update(velocity) {
-        if ($(this.div).is(':not(:hover)')) { // as long as we aren't mouseovered,
+    Post.prototype.update = function (velocity) {
+        if (!this.isHovered) { // as long as we aren't mouseovered,
             // move to new position, apply some damping to encourage stabilization,
             this.position.x += velocity.x * this.damping;
             this.position.y += velocity.y * this.damping;
 
             // and finally ensure we don't go out of bounds.
-            if (this.position.x < 0) {
-                this.position.x = 0;
-                this.velocity.x = 0;
-            }
-            if (this.position.x > this.bounds.x) {
-                this.position.x = this.bounds.x;
-                this.velocity.x = 0;
-            }
-            if (this.position.y < 0) {
-                this.position.y = 0;
-                this.velocity.y = 0;
-            }
-            if (this.position.y > this.bounds.y) {
-                this.position.y = this.bounds.y;
-                this.velocity.y = 0;
-            }
+            if (this.position.x < 0) { this.position.x = 0; this.velocity.x = 0; }
+            if (this.position.x > this.bounds.x) { this.position.x = this.bounds.x; this.velocity.x = 0; }
+            if (this.position.y < 0) { this.position.y = 0; this.velocity.y = 0; }
+            if (this.position.y > this.bounds.y) { this.position.y = this.bounds.y; this.velocity.y = 0; }
         }
 
         // redraw
         $(this.div).css({
-            'top':              this.position.y,
-            'left':             this.position.x,
+            'top':  this.position.y,
+            'left': this.position.x,
             'background-color': this.backgroundColour(),
-            'width':            this.size(),
-            'height':           this.size()
-        }).fadeTo(0, this.getOpacity());
+            'width': this.size(),
+            'height': this.size()
+        })
+            .fadeTo(0, this.getOpacity());
 
         if (this.isSelected()) {
             this.updateReplies();
         }
         return this;
     };
-    Post.prototype.updateReplies = function _updateReplies() {
-        var thisRef = this;
-        thisRef.replyList = [];
-
-        var updateCb = function _updateCb() {
-            var replyList = thisRef.replyList;
-            if (replyList.length !== 0) {
-                replyList.forEach(function (replyA) {
-                    var netVelocity = Vector2.BaseVector;
-                    replyList.forEach(function (replyB) {
-                        if (replyA !== replyB) {
-                            var repulsion = $vectorUtils.repulseFrom(replyA, replyB),
-                                attraction = Vector2.BaseVector;
-                            if ($.inArray(replyB, replyA.children) > -1 || $.inArray(replyA, replyB.children) > -1) {
-                                attraction = $vectorUtils.attractTo(replyA, replyB);
-                            }
-                            netVelocity.x += repulsion.x + attraction.x;
-                            netVelocity.y += repulsion.y + attraction.y;
-                        }
-                    });
-                    if (replyA === thisRef.replyTree) { // if root post
-                        replyA.position = thisRef.position;
+    Post.prototype.updateReplies = function () {
+        if (this.replyList === null) {
+            this.replyList = [];
+            winchatty(
+                ["ChattyService.getThreadTree", this.id],
+                getReplies,
+                function (error) {
+                    window.alert("Failed to access winchatty database: " + error);
+                }
+            );
+        } else if (this.replyList.length !== 0) {
+            var replyList = this.replyList;
+            replyList.forEach(function (replyA) {
+                var netVelocity = new Vector2(0, 0);
+                replyList.forEach(function (replyB) {
+                    if (replyA === replyB) {
+                        return;
                     }
-                    else {
-                        replyA.position.x += netVelocity.x * replyA.damping;
-                        replyA.position.y += netVelocity.y * replyA.damping;
+                    var repulsion = repulseFrom.call(replyA, replyB),
+                        attraction = new Vector2(0, 0);
+                    if ($.inArray(replyB, replyA.children) > -1 || $.inArray(replyA, replyB.children) > -1) {
+                        attraction = attractTo.call(replyA, replyB);
                     }
-                    $(replyA.div).css({
-                        'top':  replyA.position.y,
-                        'left': replyA.position.x
-                    });
+                    netVelocity.x += repulsion.x + attraction.x;
+                    netVelocity.y += repulsion.y + attraction.y;
                 });
-            }
-        };
-
-        winChattyThread.load(updateCb);
-    };
-
-    var Reply = function _Reply() {
-        this.children = [];
-        this.position = new Vector2(Math.random() * Post.prototype.bounds.x, Math.random() * Post.prototype.bounds.y);
-        this.velocity = Vector2.BaseVector;
-        this.div = jQuery('<div/>')
-            .addClass('reply')
-            .appendTo('#chatty');
-    };
-    Reply.prototype.attraction = 0.01;
-    Reply.prototype.repulsion = 600;
-    Reply.prototype.damping = 0.3;
-    // REVIEW: why a function?
-    Reply.prototype.mass = function () {
-        return 1;
+                if (replyA === this.replyTree) { // if root post
+                    replyA.position = this.position;
+                } else {
+                    replyA.position.x += netVelocity.x * replyA.damping;
+                    replyA.position.y += netVelocity.y * replyA.damping;
+                }
+                $(replyA.div).css({
+                    'top':  replyA.position.y,
+                    'left': replyA.position.x
+                });
+            });
+        }
     };
 
     // these generic-ish functions should be able to operate on both replies and posts
-    var $vectorUtils = $({
-        attractTo: function (from, to) {
-            var attraction = Vector2.BaseVector;
-            attraction.x += from.mass() * from.attraction * (to.position.x - from.position.x);
-            attraction.y += from.mass() * from.attraction * (to.position.y - from.position.y);
-            return attraction;
-        },
-        repulseFrom: function (from, to) {
-            var repulsion = Vector2.BaseVector,
-                rsq = Math.pow(this.position.x - to.position.x, 2) + Math.pow(from.position.y - to.position.y, 2);
-            if (rsq === 0) {
-                rsq = 0.00001;
-            } // careful - don't divide by zero!
-            repulsion.x = (from.repulsion * (from.position.x - to.position.x) / rsq);
-            repulsion.y = (from.repulsion * (from.position.y - to.position.y) / rsq);
-            return repulsion;
-        },
-        applyGravity: function (element) {
-            var centerOfGravity = new Vector2(element.bounds.x / 2, element.bounds.y / 2),
-                gravity = Vector2.BaseVector;
-            gravity.x = element.attraction * (centerOfGravity.x - element.position.x);
-            gravity.y = element.attraction * (centerOfGravity.y - element.position.y);
-            return gravity;
-        },
-        // Applies a force-based calculation to every post.
-        // mostly thanks to http://blog.ivank.net/force-based-graph-drawing-in-as3.html
-        postCalculationLoop: function _postCalculationLoop(posts) {
-            posts.forEach(function (postA) {
-                var netVelocity = Vector2.BaseVector;
-                posts.forEach(function (postB) {
-                    if (postA === postB) {
-                        return; // no need to compare a node to itself
-                    }
-                    var repulsion = Vector2.BaseVector,
-                        attraction = Vector2.BaseVector,
-                        gravity = $(this).applyGravity(postA, postB);
-                    if (postA.category === postB.category) {
-                        attraction = $(this).attractTo(postA, postB);
-                    }
-                    if (!postA.isSelected()) {
-                        repulsion = $(this).repulseFrom(postA, postB);
-                    }
-                    netVelocity.x += repulsion.x + attraction.x + gravity.x;
-                    netVelocity.y += repulsion.y + attraction.y + gravity.y;
-                });
-                postA.update(netVelocity);
-            });
+    var attractTo = function (that) {
+        var attraction = new Vector2(0, 0);
+        attraction.x += this.mass() * this.attraction * (that.position.x - this.position.x);
+        attraction.y += this.mass() * this.attraction * (that.position.y - this.position.y);
+        return attraction;
+    };
+    var repulseFrom = function (that) {
+        var repulsion = new Vector2(0, 0),
+            rsq = Math.pow(this.position.x - that.position.x, 2) + Math.pow(this.position.y - that.position.y, 2);
+        if (rsq === 0) { rsq = 0.00001; } // careful - don't divide by zero!
+        repulsion.x = (this.repulsion * (this.position.x - that.position.x) / rsq);
+        repulsion.y = (this.repulsion * (this.position.y - that.position.y) / rsq);
+        return repulsion;
+    };
+    var applyGravity = function () {
+        var centerOfGravity = new Vector2(this.bounds.x / 2, this.bounds.y / 2),
+            gravity = new Vector2(0, 0);
+        gravity.x = this.attraction * (centerOfGravity.x - this.position.x);
+        gravity.y = this.attraction * (centerOfGravity.y - this.position.y);
+        return gravity;
+    };
+
+
+
+    // Because I'm bad at Javascript scoping, I couldn't figure out how to put this in an anonymous function
+    // inside the winchatty call in Post.prototype.updateReplies, and I couldn't figure out how to have it
+    // be a function within Post.prototype and still be called with arguments.
+    // So it's a standalone function.
+    function getReplies(tree) {
+        // http://programmers.stackexchange.com/questions/214227/
+        var stack = [],
+            replies = [],
+            root = new Reply();
+        $.extend(root, tree.replies[0]);
+        stack.push(root);
+        for (var i = 1; i < tree.replies.length; i++) { // start at i=1 deliberately to skip root, handled above
+            var reply = new Reply(),
+                delta = tree.replies[i].depth - stack.length;
+            $.extend(reply, tree.replies[i]);
+            if (delta > 0) {
+                assert(delta === 1);
+                stack.push(stack.front().children.front());
+            }
+            while (delta < 0) {
+                stack.pop();
+                delta++;
+            }
+            stack.front().children.push(reply); // build the tree
+            replies.push(reply); // build the list
         }
-    });
+        Post.prototype.selectedPost.replyList = replies;
+        Post.prototype.selectedPost.replyTree = root;
+    }
 
     function getThreads(story) {
         var posts = [];
-        story.threads.forEach(function (thread) {
+        story.threads.forEach(function(thread) {
             var newPost = new Post();
             $.extend(newPost, thread);
             posts.push(newPost);
@@ -246,11 +286,33 @@ $(function () {
     }
 
     $('#title').text("Loading...");
-
+    winchatty(
+        ["ChattyService.getStories"],
+        function (data) {
+            var title = data[0].title,
+                story_id = data[0].story_id;
+            winchatty(
+                ["ChattyService.getStory", story_id, 1],
+                function (story) {
+                    var posts = getThreads(story);
+                    $('#title').text(title + ": " + story.story_name);
+                    window.setInterval(function () {
+                        postCalculationLoop(posts);
+                    }, framerate);
+                },
+                function (error) {
+                    window.alert("Failed to access winchatty database: " + error);
+                }
+            );
+        },
+        function (error) {
+            window.alert("Failed to access winchatty database: " + error);
+        }
+    );
 
     $('#chatty').css({
         "height": Post.prototype.bounds.y,
-        "width":  Post.prototype.bounds.x
+        "width": Post.prototype.bounds.x
     });
 
     $('input[name=repulsion]').val(Post.prototype.repulsion);
